5ded71a937983930a680660fe616a963b05db7a9
tinhpt <tinhpt.38@gmail.com>
2020-07-04 10:48:27 +0700

    update fontpage into columns 2. update admin page to remove block navigation

--- layout/admin.php
+++ layout/admin.php
@@ -4,9 +4,11 @@ defined('MOODLE_INTERNAL') || die();
 user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
 require_once($CFG->libdir . '/behat/lib.php');
 if (isloggedin()) {
+    $isloggedin = true;
     $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
 } else {
     $navdraweropen = false;
+    $isloggedin = false;
 }
 $extraclasses = [];
 if ($navdraweropen) {
@@ -14,6 +16,7 @@ if ($navdraweropen) {
 }
 $bodyattributes = $OUTPUT->body_attributes($extraclasses);
 $blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = strpos($blockshtml, 'data-block=') !== false;
 $buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions();
 // If the settings menu will be included in the header then don't add it here.
 $regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
@@ -21,10 +24,11 @@ $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
     'output' => $OUTPUT,
     'sidepreblocks' => $blockshtml,
-    // 'hasblocks' => $hasblocks,
+
     'bodyattributes' => $bodyattributes,
     'navdraweropen' => $navdraweropen,
     'regionmainsettingsmenu' => $regionmainsettingsmenu,
+    'isloggedin' => $isloggedin,
     'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu)
 ];
 
